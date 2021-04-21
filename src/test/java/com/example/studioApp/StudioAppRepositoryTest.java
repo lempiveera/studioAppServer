838fcd7e2511be3e2926dcd6636e22298758b0f6
lempiveera <bgm092@myy.haaga-helia.fi>
2021-04-22 01:01:15 +0300

    ready

--- src/test/java/com/example/studioApp/StudioAppRepositoryTest.java
+++ src/test/java/com/example/studioApp/StudioAppRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.studioApp.model.User;
@@ -20,7 +21,7 @@ import com.example.studioApp.model.TodoRepository;
 import com.example.studioApp.model.UserRepository;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest
 public class StudioAppRepositoryTest {
 
 	@Autowired
