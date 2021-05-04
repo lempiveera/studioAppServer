24da13038dca7230183b27455231114edc80f78a
lempiveera <bgm092@myy.haaga-helia.fi>
2021-05-04 19:38:31 +0300

    test

--- src/main/java/com/example/studioApp/StudioAppApplication.java
+++ src/main/java/com/example/studioApp/StudioAppApplication.java
@@ -58,6 +58,8 @@ public class StudioAppApplication {
 			presentrepo.save(you);
 			
 			
+			//this is a comment
+			
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@email.com","USER");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C","admin@email.com" ,"ADMIN");
 
